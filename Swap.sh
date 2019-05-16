c7d5431cf7c9135c9690647dff42405bc717899b
Gra8-Git <39712159+Gra8-Git@users.noreply.github.com>
2019-05-16 19:52:38 +0300

    Create Swap.sh

---
+++ Swap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+  echo "==================================CREATE SWAP============================================\n"
+if sudo swapon --show | grep -q '/dev/'; then
+  echo "==========================you have swap have anice day!!!================================\n"
+  exit 0
+else
+  echo "create swap file=========================================================================\n"
+  cd /
+  sudo fallocate -l 1G /swapfile
+  sudo chmod 600 /swapfile
+  echo "set up swap area to file=================================================================\n"
+  sudo mkswap /swapfile
+  echo "turn on swap=============================================================================\n"
+  sudo swapon /swapfile
+  echo "=========================================================================================\n"
+  sudo free -h
+  echo "how often the system will use the swap:" && cat /proc/sys/vm/swappiness
+  echo "=========================================================================================\n"
+  echo "change permanent open the /etc/fstab file and to add this line \n"
+  echo "/swapfile swap swap defaults 0 0 \n"
+  echo "/etc/sysctl.conf To change how often the system will use the swap space you need to add \n"
+  echo "this line to file /etc/sysctl.conf --->         vm.swappiness=10"
+  echo "deactivate swap : sudo swapoff -v /swapfile    and remove swap file: sudo rm /swapfile"
