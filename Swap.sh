7fdca9dc63b8e58f788c822e18eeb6c024a98383
Gra8-Git <39712159+Gra8-Git@users.noreply.github.com>
2019-05-31 22:50:20 +0300

    Update Swap.sh

--- Swap.sh
+++ Swap.sh
@@ -20,6 +20,7 @@ else
   echo "=========================================================================================\n"
   echo "change permanent open the /etc/fstab file and to add this line \n"
   echo "/swapfile swap swap defaults 0 0 \n"
+  echo "or use this command :) lazy admin: echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab \n"
   echo "/etc/sysctl.conf To change how often the system will use the swap space you need to add \n"
   echo "this line to file /etc/sysctl.conf --->         vm.swappiness=10"
   echo "deactivate swap : sudo swapoff -v /swapfile    and remove swap file: sudo rm /swapfile"
