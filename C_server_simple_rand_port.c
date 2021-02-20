e88df0be1b5d476b91c0d8dc0b5f5b915ffa47b2
Gra8-Git <39712159+Gra8-Git@users.noreply.github.com>
2021-02-20 23:53:58 +0200

    Update C_server_simple_rand_port.c

--- C_server_simple_rand_port.c
+++ C_server_simple_rand_port.c
@@ -1,4 +1,3 @@
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -6,24 +5,33 @@
 //#define INADDR_LOOPBACK     0x7f000001
 //#define INADDR_ANY      0x00000000
 void read_socket(int newsock, int pid);
+void server_main();
 void main(){
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-        struct sockaddr_in sock_addr;
+server_main();
+}
+
+void server_main(){
+       int sock = socket(AF_INET, SOCK_STREAM, 0);
+        struct sockaddr_in sock_addr,port_addr;
         int pid;
         int newsock;
         sock_addr.sin_family = AF_INET;
         sock_addr.sin_port = htons(0);
         sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
         bind(sock,(struct sockaddr *)&sock_addr , sizeof(sock_addr));
+
+        int len = sizeof(port_addr);
+        getsockname(sock, (struct sockaddr *) &port_addr, &len);
+        printf("%d",ntohs(port_addr.sin_port));
+
         listen(sock,5);
         int scli=sizeof(sock_addr);
 while(newsock = accept(sock, (struct sockaddr *) &sock_addr, &scli)){
    int pid;
     if((pid = fork()) == 0) {
-read_socket(newsock,pid);
-}
-}
-}
+        read_socket(newsock,pid);
+}}}
+
 void read_socket(int newsock, int pid)
 {
 char buffer[256];
@@ -31,4 +39,5 @@ while(read(newsock,buffer,255)>0){
 printf(buffer);
 }
 }
+
 
